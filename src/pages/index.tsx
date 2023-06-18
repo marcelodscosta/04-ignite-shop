035939db554f0dbdd7c5292920def5f130a9f600
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-18 20:03:49 -0300

    Configurando Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,16 @@
+import { styled } from '../styles';
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius: 8,
+  padding: '8px 6px',
+  border: 0,
+
+  '&:hover': {
+    filter: 'brightness(0.8)',
+  },
+});
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return <Button onClick={() => alert('Teste')}>Hello World</Button>;
 }
