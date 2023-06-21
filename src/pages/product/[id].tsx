2dcbee1591bf427aa8c01e3793d6c95c2e2176f2
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-21 13:59:51 -0300

    Estrutura Página Produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,29 @@
 import { useRouter } from 'next/router';
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product';
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Produto: {query.id} </h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camisa X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          molestias modi ipsa iste eos natus quis accusamus eveniet ad
+          reiciendis, laudantium atque delectus itaque omnis dolore incidunt
+          officiis nulla vel!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
