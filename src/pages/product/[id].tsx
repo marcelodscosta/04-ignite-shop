30b3741230ed822e3a8adab57d6334e735b3e1a2
marcelodscosta <81534727+marcelodscosta@users.noreply.github.com>
2023-06-21 16:10:22 -0300

    load dados prod ssg par. dinamico e fallback ssg

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,19 +1,32 @@
-import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import Image from 'next/image';
+import Stripe from 'stripe';
+import { stripe } from '../../lib/stripe';
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from '../../styles/pages/product';
 
-export default function Product() {
-  const { query } = useRouter();
+type ProducProps = {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+};
 
+export default function Product({ product }: ProducProps) {
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} alt="" width={520} height={480} />
+      </ImageContainer>
       <ProductDetails>
-        <h1>Camisa X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
@@ -27,3 +40,35 @@ export default function Product() {
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const productId = String(params.id);
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+  };
+};
